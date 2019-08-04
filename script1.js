9ccff3e9e996de01ac5e31bfdb4e711b86c038b7
Andrew Lazariev <LazarievAndrew@users.noreply.github.com>
2019-08-04 16:40:53 +0300

    ver.2.1

---
+++ script1.js
@@ -0,0 +1,30 @@
+// Активный квадрат
+
+// создать 10 квадратов зеленого цвета
+// при клике по клику на любой из квадратов он выделяется красным цветом,
+// если один уже выделен красным, а кликнули по-другому,
+// то выделение с прошлого — снимается и ставится на тот, по котором кликнули
+
+var redSquare;
+
+ function getSquare(event) {
+
+    var target = event.target; 
+  
+    if (target.tagName != 'TD') return;
+  
+    activClass(target); 
+  };
+
+  function activClass(td) {
+
+    if (redSquare) { 
+
+        redSquare.classList.remove('square--red');
+    }
+
+    redSquare = td;
+    redSquare.classList.add('square--red'); 
+  };
+
+  squareList.addEventListener ('click', getSquare);
